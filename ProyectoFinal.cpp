f9d77e93da767ed821b91687f715350b81507ef4
Alexter2003 <alexvasuni@gmail.com>
2023-05-31 15:22:53 -0600

    agregando sistema de movimiento de jugador

--- ProyectoFinal.cpp
+++ ProyectoFinal.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <direct.h>
 #include <stdlib.h>
+
 #define ANCHO 59
 #define ALTO 25
 using namespace std;
@@ -11,47 +12,88 @@ int anchoCasilla = (ANCHO*2)/5;
 int mitadAltoCasilla = altoCasilla/2;
 int mitadAnchoCasilla = anchoCasilla/2;
 
-void gotoxy(int x,int y)
-{
-	HANDLE hCon;
-	hCon=GetStdHandle(STD_OUTPUT_HANDLE);
-	COORD dwPos;
-	dwPos.X=x;
-	dwPos.Y=y;
-	SetConsoleCursorPosition(hCon,dwPos);
-}
-void recuadro(){
-	
-	for(int j = 0; j<ALTO ; j+=altoCasilla){
-		for(int i=0; i<ANCHO*2; i++){
-			gotoxy(i,j);
-			cout<<char(196);
-			gotoxy(i,ALTO);
-			cout<<char(196);
+	void gotoxy(int x,int y)
+	{
+		HANDLE hCon;
+		hCon=GetStdHandle(STD_OUTPUT_HANDLE);
+		COORD dwPos;
+		dwPos.X=x;
+		dwPos.Y=y;
+		SetConsoleCursorPosition(hCon,dwPos);
+	}
+	void recuadro(){
+		
+		for(int j = 0; j<ALTO ; j+=altoCasilla){
+			for(int i=0; i<ANCHO*2; i++){
+				gotoxy(i,j);
+				cout<<char(196);
+				gotoxy(i,ALTO);
+				cout<<char(196);
+			}
+		}
+		
+		for(int j=0; j<(ANCHO*2) ; j+=anchoCasilla){
+			
+			for(int i=0; i<=ALTO; i++){
+				gotoxy(j,i);
+				cout<<char(124);
+				gotoxy(ANCHO*2-3,i);
+				cout<<char(124);
+			}
+			
 		}
 	}
-	
-	for(int j=0; j<(ANCHO*2) ; j+=anchoCasilla){
-		for(int i=0; i<=ALTO; i++){
-			gotoxy(j,i);
-			cout<<char(124);
-			gotoxy(ANCHO*2-3,i);
-			cout<<char(124);
-		}
-	}
-}
 	
 	void insertarJugador(int altoCasilla, int anchoCasilla){
 		gotoxy( anchoCasilla, altoCasilla);
 		cout<<char(220);
 	}
 		
-		int main()
-		{
-			system("cls");
-			system("color 20");
-			insertarJugador(mitadAltoCasilla + (altoCasilla)*0,mitadAnchoCasilla);
-			recuadro();
+	
+		
+	int main()
+	{
+		
+		system("cls");
+		system("color 20");
+		string direccion;
+		int movimientoHorizontal = 0;
+		int movimientoVertical = 0;
+		bool esIgual;
+		
+		insertarJugador(mitadAltoCasilla + (altoCasilla)*0,mitadAnchoCasilla + (anchoCasilla)*0);
+		recuadro();
+		
+		while(true){
 			
-			return 0;
+			cout<<"\nEscriba una direccion: ";
+			cin>>direccion;
+			
+			if(direccion == "s"){
+				movimientoVertical++;
+				system("cls");
+				insertarJugador(mitadAltoCasilla + (altoCasilla)*movimientoVertical,mitadAnchoCasilla + (anchoCasilla)*movimientoHorizontal);
+				recuadro();
+				
+			} else if(direccion == "w"){
+				movimientoVertical--;
+				system("cls");
+				insertarJugador(mitadAltoCasilla + (altoCasilla)*movimientoVertical,mitadAnchoCasilla + (anchoCasilla)*movimientoHorizontal);
+				recuadro();
+				
+			} else if(direccion == "d"){
+				movimientoHorizontal++;
+				system("cls");
+				insertarJugador(mitadAltoCasilla + (altoCasilla)*movimientoVertical,mitadAnchoCasilla + (anchoCasilla)*movimientoHorizontal);
+				recuadro();
+			}	else if(direccion == "a"){
+				movimientoHorizontal--;
+				system("cls");
+				insertarJugador(mitadAltoCasilla + (altoCasilla)*movimientoVertical,mitadAnchoCasilla + (anchoCasilla)*movimientoHorizontal);
+				recuadro();
+			}
 		}
+		
+		
+		return 0;
+	}
